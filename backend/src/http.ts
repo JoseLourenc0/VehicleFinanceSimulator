a46c381223b3c84735415abb7c8ba8402ecf223f
joselourenco <3s3phinxil@gmail.com>
2024-01-07 09:11:43 -0300

    feat: back-middleware limiter

--- backend/src/http.ts
+++ backend/src/http.ts
@@ -2,13 +2,14 @@ import cors from 'cors'
 import express from 'express'
 import signinRouter from './routes/signin.route'
 import vehicleRouter from './routes/vehicle.route'
+import limiter from './middlewares/rate-limiter'
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
-app.use('/sign-in', signinRouter)
-app.use('/vehicles', vehicleRouter)
+app.use('/sign-in', limiter, signinRouter)
+app.use('/vehicles', limiter, vehicleRouter)
 
 export { app }
